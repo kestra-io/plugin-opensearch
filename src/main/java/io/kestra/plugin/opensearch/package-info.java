e0bacf4f02af2be02f3aa5b03334624076293dfb
François Delbrayelle <fdelbrayelle@gmail.com>
2026-02-13 08:41:54 +0100

    chore: update plugin categories (#29)

--- src/main/java/io/kestra/plugin/opensearch/package-info.java
+++ src/main/java/io/kestra/plugin/opensearch/package-info.java
@@ -1,7 +1,9 @@
 @PluginSubGroup(
     description = "This sub-group of plugins contains tasks for using Opensearch.\n" +
-        "Opensearch is a distributed, RESTful search and analytics engine capable of addressing a growing number of use cases.",
-    categories = PluginSubGroup.PluginCategory.DATABASE
+        "Opensearch is a distributed, RESTful search and analytics engine capable of addressing a growing number of use cases.",categories = {
+        PluginSubGroup.PluginCategory.DATA,
+        PluginSubGroup.PluginCategory.INFRASTRUCTURE
+    }
 )
 package io.kestra.plugin.opensearch;
 
